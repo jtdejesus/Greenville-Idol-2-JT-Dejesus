a3139f73b6590e978b869c70a60fa868ce89f673
jtdejesus <157234255+jtdejesus@users.noreply.github.com>
2024-07-01 00:02:33 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+     int prevyearcontestants=679;
+     int curryearcontestants=252;
+
+   
+   
+    if (curryearcontestants< prevyearcontestants) Console.WriteLine("A tighter race this year!  Come out and cast your vote!");
+  } 
+}
